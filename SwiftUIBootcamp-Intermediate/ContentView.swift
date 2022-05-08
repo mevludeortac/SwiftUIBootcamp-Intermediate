93f7bcb459ffbbe87e5a5acfbf72e4eb782e123b
goldbilgisayar <ortacmevlude@gmail.com>
2022-05-08 15:40:21 +0300

    Initial Commit

---
+++ SwiftUIBootcamp-Intermediate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBootcamp-Intermediate
+//
+//  Created by MEWO on 8.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBootcamp-Intermediate/SwiftUIBootcamp_IntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcamp_IntermediateApp.swift
+//  SwiftUIBootcamp-Intermediate
+//
+//  Created by MEWO on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcamp_IntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
